8f94dfcf03814178d104f77c8296a29cf94b9359
Erik Montes <erikmontesdev@gmail.com>
2022-09-20 21:03:45 -0700

    add js data type exmpales

--- js/script.js
+++ js/script.js
@@ -1,3 +1,85 @@
 
 
 // alert("it works")
+
+
+
+// White Space
+// Case Sensitivity
+// Comments
+// Semicolons
+
+
+/* function
+for
+let
+var
+const
+switch */
+
+console.log('Hello there');
+console.log("this is another one!!!");
+console.log(`i like 🍕`);
+
+
+// Data Types (Categories) (Category Types)
+
+/*
+PRIMITIVES
+- Numbers - all numbers between off -9007199254740991 and 9007199254740991  i.e. 5, 2.4, 0, -8, -1244.34, 55555555, NaN, Infinity
+- Strings - text, words, sentences, think of a letter necklaces
+    - "" double quotes
+    - '' single quotes
+    - `` backticks
+- Booleans - true and false (yes and no) ( on and off) (1 and 0)
+- Null - null itself (nothingness)
+- Undefined - like a placeholder to be discovered
+
+- Symbol - a unique value that gets for you
+- Big Int(eger) - like numbers but allows for more massive values denoted with an n at the end i.e  100n, 5n
+
+COMPLEX
+- array - a list of primitives
+- object - is structure that allows to describe an "object" using primitives and describe the actions (functions) that the "object" can do
+*/
+
+/*
+array example
+
+Colors
+- "red"
+- "blue"
+- "green"
+
+Pets
+- "dogs"
+- "cats"
+- "ducks"
+- "chickens"
+
+Weekly Temperature
+- 100
+- 99
+- 101
+- 89
+- 88
+
+
+
+
+*/
+
+
+/*
+object example
+-------
+adjectives (properties)
+    name -> "Erik"
+    role -> "instructor"
+    lucky number -> 7
+    is happy -> true
+verbs (actions)
+    run -> function
+    eat -> function
+    jump -> function
+*/
